533f624df68c781e52e57e3f172b0a4d75f89486
vagrant <vagrant@homestead>
2017-02-25 23:09:40 +0000

    another deploy test

--- resources/assets/js/components/drops.vue
+++ resources/assets/js/components/drops.vue
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div>
-                <h1>Welcome to Texas Jobs Portalsssssxxx</h1>
+                <h1>Welcome to Texas Jobs Portal</h1>
             </div>
             <div>
                 <div>
